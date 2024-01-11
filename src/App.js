163d6dddf99008605f3784a6447963acd6821ff3
royboot <rgrizell467@gmail.com>
2024-01-11 22:12:49 +0100

    Sass
    added the Sass Framework and used it to make a basic template for the navbar

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul className = "FlexBox">
+        <li className = "Ln"><a className = "Nl">basics</a></li>
+        <li className = "Ln"><a className =  "Nl">documentation</a></li>
+        <li className = "Ln"><button className = "Switch"></button></li>
+      </ul>
+    </>
+
   );
 }
-
 export default App;
